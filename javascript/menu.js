3e148b0258838bea0bce0d732a2fcd8bdde06c3b
ramantra <rmroomkh70@gmail.com>
2024-05-23 15:09:26 +0200

    js

--- javascript/menu.js
+++
@@ -1,9 +0,0 @@
-// javascript/menu.js
-document.addEventListener('DOMContentLoaded', function() {
-    const burger = document.querySelector('.burger');
-    const navLinks = document.querySelector('.nav-links');
-
-    burger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
